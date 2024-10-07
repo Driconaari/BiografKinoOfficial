59c267457a5053e469b123dbed3f914903c57fdf
asger <asgervb@gmail.com>
2024-10-07 13:17:20 +0200

    trying to fix database and display on website

--- src/main/java/com/example/biografkinoofficial/entity/Movie.java
+++ src/main/java/com/example/biografkinoofficial/entity/Movie.java
@@ -1,13 +1,11 @@
 package com.example.biografkinoofficial.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.util.Date;
 
 @Entity
+@Table(name = "movie")
 public class Movie {
 
   @Id
