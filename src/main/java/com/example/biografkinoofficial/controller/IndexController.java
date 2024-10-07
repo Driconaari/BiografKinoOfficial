9f58b9f06646af07567b20d8c670a4ac7fee9614
asger <asgervb@gmail.com>
2024-10-07 13:38:07 +0200

    still trying to show the list

--- src/main/java/com/example/biografkinoofficial/controller/IndexController.java
+++ src/main/java/com/example/biografkinoofficial/controller/IndexController.java
@@ -1,13 +1,24 @@
 package com.example.biografkinoofficial.controller;
 
+import com.example.biografkinoofficial.entity.Movie;
+import com.example.biografkinoofficial.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private MovieService movieService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        List<Movie> movies = movieService.getAllMovies();
+        model.addAttribute("movies", movies);
         return "index";
     }
 }
